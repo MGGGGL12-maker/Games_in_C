39bbe26c11fc0ee97355a9ad07ba649b6e85c973
Joao MIguel <jmocarvallho7123@gmail.com>
2025-07-27 10:05:22 -0300

    Consertando Projeto do Final Fantasy

--- Simple_Bakery_System/bakery.c
+++ Simple_Bakery_System/bakery.c
@@ -2,7 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+typedef struct
+{
+    char name[101];
+    int prod_cod;
+    int qtd;
+    int n_sells;
+    int n_buys;
+    float price;
+    int status; // 0 -> Whithout the System 1 -> Whithin the System
+}Product;
+
+
+void print_Products(Product* shelf, int tam)
+{
+    for (int prod = 0; prod<tam; prod++)
+    {
+        printf("\nNome: %s | Preço: %.2f | Código: %i\n", shelf[prod].name, shelf[prod].price, shelf[prod].prod_cod);
+    }
+}
+
+
 int main()
 {
+    // Variáveis auxiliares
+    int tam = 10;
+
+    // Vetores auxiliares
+    Product shelf[tam];
+
+
+
+
+    print_Products(shelf, tam);
     return 0;
 }
